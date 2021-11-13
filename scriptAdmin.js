b490125bca686d2a96fd1e0c0ab97a98624b3cee
Bartucz <43059630+Bartucz@users.noreply.github.com>
2021-11-13 10:37:50 +0100

    children

---
+++ scriptAdmin.js
@@ -0,0 +1,20 @@
+$(function() {
+
+    myAjax("termekek.json", termekLista);
+   // const TermekTabla=[];
+
+    function termekLista(termekek) {
+        const szuloElem = $(".termekek");
+        const sablonElem = $(".termek");
+
+        termekek.forEach(function(elem) {
+            let node = sablonElem.clone().appendTo(szuloElem);
+
+            const obj = new TermekAruhaz(node, elem);
+           // TermekTabla.push(obj);//példányok táblába
+
+        });
+        sablonElem.remove(); //sablonelem eltávolítása
+    }
+
+});
